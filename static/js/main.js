3c58562baa47812a208dd53a225cd58599ba8eb0
Evelyn-McDonald <evel.mcdonald@gmail.com>
2018-05-03 12:24:01 -0400

    add tiny icons to feature section 3

--- static/js/main.js
+++ static/js/main.js
@@ -4,8 +4,17 @@
 var faqItem = document.querySelectorAll('#faq .q')
 
 faqItem.forEach(function(i) {
-	i.addEventListener('click', function(e) {
-		console.log(e)
-		e.target.closest('li').classList.toggle("active")
-	})
+    i.addEventListener('click', function(e) {
+        let li = e.target.closest('li')
+
+        if (li.classList.length) {
+            li.classList.remove("active")
+            e.target.querySelector('.fa-plus').style.display = "block"
+            e.target.querySelector('.fa-minus').style.display = "none"
+        } else {
+            li.classList.add("active")
+            e.target.querySelector('.fa-plus').style.display = "none"
+            e.target.querySelector('.fa-minus').style.display = "block"
+        }
+    })
 })
