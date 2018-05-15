c9a455ff4855268ee6eefdf9775f87564118ff31
Evelyn-McDonald <evel.mcdonald@gmail.com>
2018-05-14 21:47:59 -0400

    fix mobile menu links

--- static/js/main.js
+++ static/js/main.js
@@ -7,6 +7,12 @@ mobileToggle.addEventListener("click", function() {
     document.body.classList.toggle('open')
 })
 
+document.querySelectorAll('header .nav-links a').forEach(function(a) {
+    a.addEventListener("click", function() {
+        document.body.classList.remove('open')
+    })
+})
+
 // -------------------------------------
 // FAQ accordion
 // -------------------------------------
